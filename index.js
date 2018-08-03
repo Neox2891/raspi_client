77ee961d2de36224115679ba9feb4e9c2b107a4a
neox2891 <n.estrada2891@gmail.com>
2018-08-02 23:30:46 -0500

    agragando funcionalidades a las notificaciones

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var serialPort = new SerialPort("/dev/ttyACM0", {
     baudrate: 9600,
     parser: Readline("\r\n")
 });
+
+let humidity = false;
 // const board = new five.Board({
 //   io: new Raspi()
 // });
@@ -61,21 +63,76 @@ serialPort.on("open", function() {
 
     serialPort.on('data', function(data) {
         //console.log('data received: ' + data);
+        let arrH = [];
+        let objG = {};
         try {
             let dataRecived = JSON.parse(data);
 
             for (let props in dataRecived) {
-                if (dataRecived[props].temperature > 34) {
+
+                if (dataRecived[props].temperature > 34 || dataRecived[props].temperature < 20) {
                     socket.emit('nfTemperature', {
                         module: props,
                         temperature: dataRecived[props].temperature
                     });
                 }
 
-                if (dataRecived[props].humidity > 52) {
-                    socket.emit('nfHumidity', {
+
+                arrH.push(dataRecived[props].humidity);
+
+                if (arrH.length > 4 && humidity === true) {
+                    arrH = [];
+                }
+
+                if (arrH.length === 4 && humidity == false) {
+                    arrH.forEach((element, index) => {
+                        if (element > 52) {
+                            socket.emit('nfHumidity', {
+                                module: index + 1,
+                                humidity: element
+                            }, (cb) => console.log(cb));
+                            humidity = true;
+                        }
+                        if (element < 30) {
+                            socket.emit('nfHumidity', {
+                                module: index + 1,
+                                humidity: element
+                            }, (cb) => console.log(cb));
+                            humidity = true;
+                        }
+                    });
+                    arrH = [];
+                }
+
+                console.log(arrH);
+
+                if ((arrH[0] <= 51 && arrH[0] > 30) &&
+                    (arrH[1] <= 51 && arrH[1] > 30) &&
+                    (arrH[2] <= 51 && arrH[2] > 30) &&
+                    (arrH[3] <= 51 && arrH[3] > 30)) {
+                    humidity = false;
+                }
+
+                console.log(humidity);
+
+                if (dataRecived[props].ammonia > 600) {
+                    socket.emit('nfAmmonia', {
                         module: props,
-                        temperature: dataRecived[props].humidity
+                        ammonia: dataRecived[props].ammonia
+                    });
+                }
+
+                if (dataRecived[props].fire == 1) {
+                    socket.emit('nfFire', {
+                        module: props,
+                        fire: dataRecived[props].fire
+                    });
+                }
+
+                if (dataRecived[props].rain == 1) {
+                    socket.emit('nfRain', {
+                        module: props,
+                        rain: dataRecived[props].rain
                     });
                 }
             }
