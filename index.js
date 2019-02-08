666dad3357a6ac4490fbc154f739c88edad90df9
neox2891 <n.estrada2891@gmail.com>
2019-02-08 16:48:37 -0500

    add config actuators

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const five = require('johnny-five');
 const SerialPort = require('serialport');
 const request = require('request');
 
-const socket = require('socket.io-client')('https://csa-systems.herokuapp.com');
+const socket = require('socket.io-client')('http://192.168.1.8:8080');
 
 var Readline = SerialPort.parsers.readline;
 
@@ -14,6 +14,7 @@ var serialPort = new SerialPort("/dev/ttyUSB0", {
 
 let counter = 0;
 let temperature = false;
+// notificaciones
 let maxTemp = 0, minTemp = 0,
     maxHum = 0, minHum = 0,
     maxAir = 0;
@@ -40,7 +41,7 @@ let getMinutes = () => {
 function enviarDatos(data) {
   
   let options = {
-    url: 'http://192.168.1.6:3000/referenceValues',
+    url: 'http://192.168.1.8:8080/referenceValues',
     method: 'GET',
     headers: {
       'Content-Type': 'application/json'
@@ -57,42 +58,107 @@ function enviarDatos(data) {
     
     console.log(referenceValues);
     
-    let obj = {
-      aT: referenceValues.maxTemp,
-      iT: referenceValues.minTemp,
-      aH: referenceValues.maxHum,
-      iH: referenceValues.minHum,
-      aA: referenceValues.maxAir,
-      A: [referenceValues.actuators[0], referenceValues.actuators[1], referenceValues.actuators[2], referenceValues.actuators[3]]
-    };
+    if (referenceValues.flag === true) {
       
-    console.log(obj);
-       
-       serialPort.write(JSON.stringify(obj), function(err) {
-          if (err) {
-            return console.log('Error on write: ', err.message);
-          }
-          console.log('message written');
-        });
-        // Open errors will be emitted as an error event
-        serialPort.on('error', function(err) {
-          console.log('Error: ', err.message);
-          });
+      maxTemp = referenceValues.maxTemp, minTemp = referenceValues.minTemp,
+      maxHum = referenceValues.maxHum, minHum = referenceValues.minHum,
+      maxAir = referenceValues.maxAir;
+    
+      let obj = {
+        aT: referenceValues.maxTemp,
+        iT: referenceValues.minTemp,
+        aH: referenceValues.maxHum,
+        iH: referenceValues.minHum,
+        aA: referenceValues.maxAir,
+        A: [referenceValues.actuators[0], referenceValues.actuators[1], referenceValues.actuators[2], referenceValues.actuators[3]]
+      };
+      
+      console.log(obj);
+      
+      serialPort.write(JSON.stringify(obj), function(err) {
+        if (err) {
+          return console.log('Error on write: ', err.message);
+        }
+        console.log('message written');
+      });
+      // Open errors will be emitted as an error event
+      serialPort.on('error', function(err) {
+        console.log('Error: ', err.message);
+      });
+      
+      request({
+        url: 'http://192.168.1.8:8080/referenceValues',
+        method: 'POST',
+        body:{
+          maxTemp,
+          minTemp,
+          maxHum,
+          minHum,
+          maxAir,
+          actuators: obj.A,
+          flag: false
+        },
+        json:true
+        
+      }, (err, res, body) => {
+        console.log("DENTRO" +  JSON.stringify(body));
+      });
+    }
         
   });
 }
 
-var external_data;
+let external_data;
 
 serialPort.on("open", function() {
   
     console.log('open');
 
     serialPort.on('data', function(data) {
-        console.log('data received: ' + data);
-        let dataRecived = JSON.parse(data);
+        
         try {
+          console.log('data received: ' + data);
+          let dataRecived = JSON.parse(data);
+            // bandera
+            if (dataRecived.actuadores[0] === true) {
+              request({
+              url: 'http://192.168.1.8:8080/referenceValues',
+              method: 'POST',
+              body:{
+                maxTemp,
+                minTemp,
+                maxHum,
+                minHum,
+                maxAir,
+                actuators: [1,0,0,0],
+                flag: false
+              },
+              json:true
+              
+              }, (err, res, body) => {
+                console.log("Luz" +  JSON.stringify(body));
+              });
+            }
             
+            if (dataRecived.actuadores[1] === true) {
+              request({
+              url: 'http://192.168.1.8:8080/referenceValues',
+              method: 'POST',
+              body:{
+                maxTemp,
+                minTemp,
+                maxHum,
+                minHum,
+                maxAir,
+                actuators: [0,1,0,0],
+                flag: false
+              },
+              json:true
+              
+              }, (err, res, body) => {
+                console.log("Ventilador" +  JSON.stringify(body));
+              });
+            }
             // NOTIFICACION TEMPERATURA
             // if (dataRecived && temperature === false) {
             //     dataRecived.temperature.forEach((element, index) => {
@@ -118,7 +184,7 @@ serialPort.on("open", function() {
             // console.log(temperature);
 
             external_data = data;
-            
+            realDataSend(external_data);
             if ((getMinutes() === 0 || getMinutes() === 30) && (counter === 0)) {
               socket.emit('dataDb', data, (callback) => {
               // console.log(callback);
@@ -136,9 +202,9 @@ serialPort.on("open", function() {
     });
 });
 
-setInterval(() => {
-    realDataSend(external_data);
-}, 60000);
+// setInterval(() => {
+//     realDataSend(external_data);
+// }, 5000);
 
 setInterval(() => {
     enviarDatos();
